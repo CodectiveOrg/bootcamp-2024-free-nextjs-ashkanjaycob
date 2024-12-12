ae82e767dbb94cde1f022f936f0d4be6c539d396
Ashkan Yaghobi <ashkan.yaghobii@gmail.com>
2024-12-13 00:49:38 +0330

    Added Dynamic Route Example

---
+++ src/app/about/page.tsx
@@ -0,0 +1,9 @@
+import { NextPage } from 'next'
+
+interface Props {}
+
+const Page: NextPage<Props> = ({}) => {
+  return <div>درباره ی ما</div>
+}
+
+export default Page
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,29 @@
-export default function Home() {
-  return <h1>سلام دنیا !</h1>;
+export default async function Home() {
+  
+  type User = {
+    id: number;
+    name: string;
+    username: string;
+  };
+
+  const res = await fetch("https://jsonplaceholder.typicode.com/users", {
+    cache: "no-store",
+  });
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch users");
+  }
+
+  const users: User[] = await res.json();
+
+  return (
+    <div>
+      <h1>سلام دنیا!</h1>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>{user.username}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
