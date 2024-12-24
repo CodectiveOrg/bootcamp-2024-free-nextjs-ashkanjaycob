4af0152b5c8839be1abbdc314b280750753ec309
Ashkan Yaghobi <ashkan.yaghobii@gmail.com>
2024-12-24 23:19:35 +0330

    Update Home Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,27 @@
-export default async function Home() {
-  return <h1>سلام دنیا!</h1>;
+import { ReactElement } from "react";
+
+import GlobalSearchBoxComponent from "@/components/global-search-box/global-search-box.component";
+
+import TabibLog from "@/assets/logo/TabibPic.png";
+
+import styles from "./page.module.css";
+import Image from "next/image";
+
+export default function Home(): ReactElement {
+  return (
+    <div className={styles.home}>
+      <h1>
+        <Image className={styles.logo} src={TabibLog} alt="tabib" />
+        طبیب
+      </h1>
+      <GlobalSearchBoxComponent />
+      <div className={styles.history}>
+        <div className={styles.title}>آخرین جستجوهای شما</div>
+        <ul>
+          <li>ارتوپد</li>
+          <li>قلب و عروق</li>
+        </ul>
+      </div>
+    </div>
+  );
 }
