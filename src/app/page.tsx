62e628134e8ddac00d334dfdb2467790a93ba9cb
Ashkan Yaghobi <ashkan.yaghobii@gmail.com>
2024-12-24 17:24:14 +0330

    Unnecessary Codes & Files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,3 @@
-type User = {
-  id: number;
-  username: string;
-};
-
 export default async function Home() {
-  const res = await fetch("https://jsonplaceholder.typicode.com/users", {
-    cache: "no-store",
-  });
-
-  if (!res.ok) {
-    throw new Error("Failed to fetch users");
-  }
-
-  const users: User[] = await res.json();
-
-  return (
-    <div>
-      <h1>سلام دنیا!</h1>
-      <ul>
-        {users.map((user) => (
-          <li key={user.id}>{user.username}</li>
-        ))}
-      </ul>
-    </div>
-  );
+  return <h1>سلام دنیا!</h1>;
 }
