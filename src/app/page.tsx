40ab00d01f218576c0107c4bbb6ba98b18b2ad52
Ashkan Yaghobi <ashkan.yaghobii@gmail.com>
2025-01-03 14:27:42 +0330

    Added Search Route

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,7 +20,8 @@ export default function Home(): ReactElement {
         <div className={styles.title}>آخرین جستجوهای شما</div>
         <ul>
           <li>ارتوپد</li>
-          <li>قلب و عروق</li>
+          <li>قلب و عروق</li> 
+          
         </ul>
       </div>
     </div>
---
+++ src/app/search/page.tsx
@@ -0,0 +1,7 @@
+import { NextPage } from "next";
+
+const SearchPage: NextPage = () => {
+  return <div>جست و جو</div>;
+};
+
+export default SearchPage;
